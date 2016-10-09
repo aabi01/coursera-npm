03cd44ad881019720a6547645fc3544de2e7d460
Abdullah A Malik <abdullah.amalik07@gmail.com>
2016-10-09 21:50:38 +0500

    added border and font color

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,20 +7,30 @@
     function LunchCheckController($scope) {
         $scope.msg = '';
         $scope.items = '';
+        $scope.color = 'black';
 
         $scope.checkLunch = function () {
 
             if ($scope.items == "") {
                 $scope.msg = 'Please Enter Data First';
+                $scope.color= 'red';
             } else {
                 var items = $scope.items.split(",");
 
-                var itemsNum = items.length;
+                var itemsNum = 0;
+                
+                for(var i=0; i<items.length; i++){
+                    if(items[i].trim() != ""){
+                        itemsNum++;
+                    }
+                }
                 
                 if (itemsNum <= 3) {
                     $scope.msg = 'Enjoy!';
+                    $scope.color= 'green';
                 } else {
                     $scope.msg = 'Too Much!';
+                    $scope.color= 'green';
                 }
             }
         };
