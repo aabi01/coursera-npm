8f7e16fbcae54aeafe650f16498ccaeb7250a4a5
Abdullah A Malik <abdullah.amalik07@gmail.com>
2016-10-09 21:37:05 +0500

    added field notice

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,27 +1,28 @@
 (function () {
     angular.module('LunchChecker', [])
 
-            .controller('LunchCheckerController', function ($scope) {
-                $scope.msg = '';
-                $scope.items = '';
-        
-                $scope.checkLunch = function(){
-                    
-                    if($scope.items == ""){
-                        $scope.msg = 'Please Enter Data First';
-                    }
-                    else{
-                        var items = $scope.items.split(",");
-                    
-                        var itemsNum = items.length;
-                        console.log(items);
-                        if(itemsNum <= 3){
-                            $scope.msg = 'Enjoy!';
-                        }
-                        else{
-                            $scope.msg = 'Too Much!';
-                        }
-                    }
-                };
-            });
+            .controller('LunchCheckerController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.msg = '';
+        $scope.items = '';
+
+        $scope.checkLunch = function () {
+
+            if ($scope.items == "") {
+                $scope.msg = 'Please Enter Data First';
+            } else {
+                var items = $scope.items.split(",");
+
+                var itemsNum = items.length;
+                
+                if (itemsNum <= 3) {
+                    $scope.msg = 'Enjoy!';
+                } else {
+                    $scope.msg = 'Too Much!';
+                }
+            }
+        };
+    }
 })();
