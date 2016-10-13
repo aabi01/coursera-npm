aea65b4149433b7913ce4e1b5069fe86e07f47ea
Abdullah A Malik <abdullah.amalik07@gmail.com>
2016-10-13 16:07:08 +0500

    Added angular and app.js files

---
+++ module2-solution/app.js
@@ -0,0 +1,8 @@
+angular.module('ShoppingList', [])
+        .controller('ShoppingList1Controller', ShoppingList1Controller)
+        .controller('ShoppingList2Controller', ShoppingList2Controller);
+
+ShoppingList1Controller.$inject = ['$scope'];
+ShoppingList1Controller = function(){
+    
+}
