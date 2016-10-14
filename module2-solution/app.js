dd94e2d0a29456307305cc5bc06f53387c213556
Abdullah A Malik <abdullah.amalik07@gmail.com>
2016-10-14 08:06:32 +0500

    module2 assignment functionality

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,8 +1,65 @@
 angular.module('ShoppingList', [])
-        .controller('ShoppingList1Controller', ShoppingList1Controller)
-        .controller('ShoppingList2Controller', ShoppingList2Controller);
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListCheckoffService', ShoppingListCheckoffService);
 
-ShoppingList1Controller.$inject = ['$scope'];
-ShoppingList1Controller = function(){
+// ToBuy Controller
+ToBuyController.$inject = ['ShoppingListCheckoffService'];
+function ToBuyController(ShoppingListCheckoffService){
+    tobuy = this;
     
+    tobuy.toBuyList = ShoppingListCheckoffService.getToBuyList();
+    
+    //Remove Item
+    tobuy.removeItem = function(itemIndex){
+        ShoppingListCheckoffService.removeItem(itemIndex);
+    }
+}
+
+
+//Already Bought Controller
+AlreadyBoughtController.$inject= ['ShoppingListCheckoffService'];
+function AlreadyBoughtController(ShoppingListCheckoffService){
+    alreadybought = this;
+    
+    alreadybought.alreadyBoughtList = ShoppingListCheckoffService.getBoughtList();
+}
+
+function ShoppingListCheckoffService(){
+    service = this;
+    
+    //To Buy List
+    toBuyList = [
+        {
+            itemName: 'Pepsi',
+            itemQuantity: '5 bottles'
+        },
+        {
+            itemName: 'Coke',
+            itemQuantity: '2 bottles'
+        },
+        {
+            itemName: 'Chips',
+            itemQuantity: '10 packs'
+        }
+    ];
+    
+    //AlreadyBought List
+    boughtList = [];
+    
+    service.getToBuyList = function(){
+        return toBuyList;
+    }
+    
+    service.getBoughtList = function(){
+        return boughtList;
+    }
+    
+    //Remove Item from ToBuy List
+    service.removeItem = function(itemIndex){
+        item = toBuyList[itemIndex];
+        boughtList.push(item);
+        
+        toBuyList.splice(itemIndex, 1);
+    }
 }
